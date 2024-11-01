81820087765a73c8d862c612567bfa6aca6c5beb
cardosofede <federico.cardoso.e@gmail.com>
2024-11-01 12:14:21 -0300

    (feat) update twitter client

--- services/twitter_client.py
+++ services/twitter_client.py
@@ -6,7 +6,32 @@ class TwitterClient:
         self.base_url = base_url
         self.client = httpx.AsyncClient()
 
+    async def refresh_auth(self):
+        url = self.base_url + "/api/twitter/refreshAuth"
+        response = await self.client.get(url)
+        return response.json()
+
+    async def get_me(self):
+        url = self.base_url + "/api/twitter/me"
+        response = await self.client.get(url)
+        return response.json()
+
     async def post_tweet(self, tweet: str):
-        url = self.base_url + "/api/twitter/tweet/post"
+        url = self.base_url + "/api/twitter/post"
         response = await self.client.post(url, json={"text": tweet})
+        return response.json()
+    
+    async def delete_tweet(self, tweet_id: str):
+        url = self.base_url + f"/api/twitter/delete-tweet/{tweet_id}"
+        response = await self.client.post(url)
+        return response.json()
+
+    async def get_user_tweets(self, user_id: str):
+        url = self.base_url + f"/api/twitter/tweets-by-user/{user_id}"
+        response = await self.client.get(url)
+        return response.json()
+    
+    async def search_tweets(self, query: str):
+        url = self.base_url + f"/api/twitter/search"
+        response = await self.client.get(url, params={"query": query})
         return response.json()
